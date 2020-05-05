8a43ca8bac02111a75c1b73cbe66b0996a4256d7
root <root@localhost>
2020-05-05 22:30:25 +0100

    publishing awesome pages 1588714225933

---
+++ .js
@@ -0,0 +1,3 @@
+alert("if you know how to copy a picture link and make it fit to put in html  kindly help me in the comment")
+
+alert("i mean the photo that is not provided by html,a picture link from another website pls")
